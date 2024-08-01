f4fe05fd8633f2280aebd48362365aec50edde10
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-08-01 10:57:03 -0500

    title smh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,6 +7,7 @@ import ContactMe from "./components/ContactMe";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <title>Shraddha Subash</title>
        <Navbar/>
        <div className="container mt-24 mx-auto px-12 py-4">
         <Intro/>
