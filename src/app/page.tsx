5726e4cd9cd24fc1771861fc57cee43642444967
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-08-02 22:59:29 -0500

    desc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,14 @@ import Navbar from "./components/Navbar";
 import AboutSection from "./components/AboutMe";
 import ProjectsSection from "./components/Projects";
 import ContactMe from "./components/ContactMe";
+import Head from 'next/head';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
       <title>Shraddha Subash</title>
+      <Head>
+        <meta content="My Personal Portfolio" />
+      </Head>
        <Navbar/>
        <div className="container mt-24 mx-auto px-12 py-4">
         <Intro/>
