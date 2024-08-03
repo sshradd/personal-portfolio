e4a98cc05277caf84c186ae1af27ec92a4d8d28c
Shraddha Subash <shraddhasubashpublic@gmail.com>
2024-08-02 23:07:42 -0500

    redo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,26 @@ import AboutSection from "./components/AboutMe";
 import ProjectsSection from "./components/Projects";
 import ContactMe from "./components/ContactMe";
 import Head from 'next/head';
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <title>Shraddha Subash</title>
       <Head>
-        <title>Shraddha Subash</title>
-        <meta name="description" content="My Personal Portfolio" />
-        <meta property="og:title" content="Shraddha Subash" />
-        <meta property="og:description" content="My Personal Portfolio" />
-        <meta property="og:url" content="https://www.sshradd.me/" />
-        <meta property="og:type" content="website" />
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content="Shraddha Subash" />
-        <meta name="twitter:description" content="My Personal Portfolio" />
+        <meta content="My Personal Portfolio" />
       </Head>
-      <Navbar />
-      <div className="container mt-24 mx-auto px-12 py-4">
-        <Intro />
+       <Navbar/>
+       <div className="container mt-24 mx-auto px-12 py-4">
+        <Intro/>
         <br></br>
         <br></br>
-        <AboutSection />
+        <AboutSection/>
         <br></br>
-        <ProjectsSection />
+        <ProjectsSection/>
         <br></br>
-        <ContactMe />
+        <ContactMe/>
         <br></br>
-      </div>
+        
+       </div>
     </main>
   );
 }
